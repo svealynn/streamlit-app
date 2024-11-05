b0fde6a14089ec752c133f5804ccfab24c4c8cdc
svealynn <svea.timmermanns@gmail.com>
2024-11-05 12:46:48 +0100

    new file: QR code generator

---
+++ qrcode_generator_app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import segno
+import time
+
+st.set_page_config(
+    page_title="QR CODES",
+    page_icon="👾")
+
+# add a banner
+st.image("images/main_banner.png")
+
+st.title("Welcome!")
+
+# store user input
+url = st.text_input("Please enter a URL")
+
+
+def create_qrcode(url):
+    qrcode = segno.make_qr(url)
+    qrcode.to_pil(dark="lightblue", light = "white", scale = 5, border = 1).save("images/qrcode_streamlit.png")
+
+
+if url:
+    with st.spinner("generate QR Code"):
+        time.sleep(1)
+    create_qrcode(url)
+    st.image("images/qrcode_streamlit.png",
+             caption=("Here you go <3"),
+             width = 300
+    )
+
+st.markdown(
+    "<br><hr><center>Made with 🤍 by Svea",
+    unsafe_allow_html=True)
